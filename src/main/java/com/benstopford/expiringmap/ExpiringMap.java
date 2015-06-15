52aa61ef806f3e0aecd2ce0a1bebaf63095f6e9a
Ben Stopford <benstopford@gmail.com>
2015-06-15 10:59:56 +0100

    simple javadoc

--- src/main/java/com/benstopford/expiringmap/ExpiringMap.java
+++ src/main/java/com/benstopford/expiringmap/ExpiringMap.java
@@ -2,6 +2,19 @@ package com.benstopford.expiringmap;
 
 import java.util.*;
 
+/**
+ * HashMap backed cache that provides configurable expiry.
+ *
+ * Entries will be validated for expiry on any action of the interface.
+ *
+ * Expiry times for each entry are held chronologically so that only
+ * entries that require expiry will be examined on any operation against
+ * this map.
+ *
+ * @param <K> the type of keys maintained by this map
+ * @param <V> the type of mapped values
+ */
+
 public class ExpiringMap<K, V> implements ExpireMap<K, V> {
     private Map<K, V> map = new HashMap<>();
     private Map<Long, List<K>> orderedExpiryTimes = new TreeMap<>();
@@ -22,26 +35,6 @@ public class ExpiringMap<K, V> implements ExpireMap<K, V> {
         flagForFutureExpiry(entryKey, timeoutMs);
 
         map.put(entryKey, value);
-    }
-
-    private void flagForFutureExpiry(K entryKey, long timeoutMs) {
-        long expiryTime = clock.now() + timeoutMs;
-
-        if (timeoutMs < 0) {
-            throw new IllegalArgumentException("Timeout must be a positive value");
-        }
-
-        if (expiryTime < 0) {
-            expiryTime = Long.MAX_VALUE;
-            System.err.println("Warning: timeout value too large: Rounding to Long.MAX_VALUE");
-        }
-
-        List<K> keys = orderedExpiryTimes.get(expiryTime);
-        if (keys == null)
-            keys = new ArrayList<>();
-        keys.add(entryKey);
-
-        orderedExpiryTimes.put(expiryTime, keys);
     }
 
     @Override
@@ -72,4 +65,24 @@ public class ExpiringMap<K, V> implements ExpireMap<K, V> {
         return expiry <= clock.now();
     }
 
+    private void flagForFutureExpiry(K entryKey, long timeoutMs) {
+
+        if (timeoutMs < 0)
+            throw new IllegalArgumentException("Timeout must be a positive value");
+
+        long expiryTime = clock.now() + timeoutMs;
+        if (expiryTime < 0)
+            expiryTime = Long.MAX_VALUE;
+
+        save(entryKey, expiryTime);
+    }
+
+    private void save(K entryKey, long expiryTime) {
+        List<K> keys = orderedExpiryTimes.get(expiryTime);
+        if (keys == null)
+            keys = new ArrayList<>();
+        keys.add(entryKey);
+
+        orderedExpiryTimes.put(expiryTime, keys);
+    }
 }
