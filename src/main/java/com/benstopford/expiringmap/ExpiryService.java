34725f23cf8b581e011a0bf15c100f3409a81d73
Ben Stopford <benstopford@gmail.com>
2015-06-16 17:04:54 +0100

    tidy

--- src/main/java/com/benstopford/expiringmap/ExpiryService.java
+++ src/main/java/com/benstopford/expiringmap/ExpiryService.java
@@ -11,16 +11,20 @@ public class ExpiryService<K> {
         if (head.expiry() <= clock.now()) {
             backingMap.remove(head.key());
         } else {
-            long waitTime = head.expiry() - clock.now();
-            queue.offer(head); //put it back before we wait
-            if (waitTime > 0) {
-                long ms = (long) Math.floor(waitTime / 1000000);
-                int ns = (int) Math.floor(waitTime % 1000000);
-                synchronized (WaitService.class) {
-                    //ensure head has not been replaced (with more immanently expiring value) before entering wait
-                    if (queue.peek().key().equals(head.key()))
-                        waitService.doWait(ms, ns);
-                }
+            waitForItToExpire(clock, waitService, queue, head);
+        }
+    }
+
+    private void waitForItToExpire(Clock clock, WaitService waitService, BlockingQueue<ExpiryEntry<K>> queue, ExpiryEntry head) throws InterruptedException {
+        long waitTime = head.expiry() - clock.now();
+        queue.offer(head); //put it back before we wait
+        if (waitTime > 0) {
+            long ms = (long) Math.floor(waitTime / 1000000);
+            int ns = (int) Math.floor(waitTime % 1000000);
+            synchronized (WaitService.class) {
+                //ensure head has not been replaced (with more immanently expiring value) before entering wait
+                if (queue.peek().key().equals(head.key()))
+                    waitService.doWait(ms, ns);
             }
         }
     }
